6825ed78c3a06671f6d5e6367364212173e1e2dd
curtjen <5837773+curtjen@users.noreply.github.com>
2026-03-22 19:12:16 -0600

    Move shell helper scripts under zsh module

---
+++ modules/zsh/scripts/aliases.zsh
@@ -0,0 +1,6 @@
+# Aliases pointing to gir-managed helper scripts.
+
+alias gpull='~/.gir/modules/zsh/scripts/bin/gpull'
+alias gpush='~/.gir/modules/zsh/scripts/bin/gpush'
+alias gcommit='~/.gir/modules/zsh/scripts/bin/gcommit'
+alias mvnode='~/.gir/modules/zsh/scripts/bin/mvnode'
